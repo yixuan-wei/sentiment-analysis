23a2e070cb2ac1a5be13e65311f3778f42a03310
卫艺璇 <weiyixuan@weiyixuandeMacBook-Pro.local>
2017-11-05 10:16:58 +0800

    fine

--- process.py
+++ process.py
@@ -3,13 +3,14 @@ import xlrd
 import requests
 import re
 
-non_en_pattern = re.compile('[\u0100-\uffff]+')
-data = xlrd.open_workbook('qqMusic1.xlsx')
+# define english words as latin-1 + common punctuations + Super/Sub scripts + currency + math symbol
+non_en_pattern = re.compile('([\u0300-\u1cdf]|[\u1f00-\u1fff]|[\u2b00-\ua6ff]|[\ua800-\ufe1f]|[\ufe30-\uffff])+')
+data = xlrd.open_workbook('FemaleA.xlsx')
 sheet = data.sheets()[0]  # load the first sheet
 data0 = sheet.col_values(0)  # load the first column data
 
 
-def is_english(string):
+def is_non_english(string):
     """判断一个utf-8字符串是否为英文合法字符"""
     match = non_en_pattern.search(string)
     return match  # true: non english exist
@@ -31,8 +32,10 @@ def process_sentiment(lyric):
     res = requests.post('https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21',
                         auth=authen, data=lyric, headers=headers1)
     result = res.text
+    res.close()
+    print(result)
     song_tone = []
-    pattern1 = re.compile('\"score\":[01]\\.[0-9]+')
+    pattern1 = re.compile('\"score\":[01]\.[0-9]+')
     pattern2 = re.compile('\"tone_id\":\"[A-Za-z]+\"')
     scores = pattern1.findall(result)
     ids = pattern2.findall(result)
@@ -53,20 +56,22 @@ def process_sentiment(lyric):
 if __name__ == '__main__':
 
     for line in data0:
-        if is_english(line) or u'Written by' in line:
+        if 'Written by' in line:
+            continue
+        if is_non_english(line):
             flag = False
             continue
 
         if ' - ' in line:
             if sing_name != "":
                 if flag:
-                    lyrics.encode('utf-8').decode('latin-1')
+                    #lyrics.encode('utf-8').decode('latin-1')
                     song_tone = process_sentiment(lyrics)
                     info.append([sing_name, singer, song_tone])
                 sing_name = ""
                 lyrics = ""
                 singer = []
-                flag = True
+            flag = True
             new_line = line.split(' - ')
             sing_name = new_line[0]
             singers = new_line[1]
@@ -88,14 +93,14 @@ if __name__ == '__main__':
                 out.write(j)
                 if punc1 > 0:
                     out.write(", ")
-                    punc = punc1 - 1
+                    punc1 = punc1 - 1
             out.write("], [")
             punc2 = len(i[2])-1  # to calculate numbers of punctuation
             for j in i[2]:
                 out.write(j)
                 if punc2>0:
                     out.write(", ")
-                    punc = punc2-1
+                    punc2 = punc2-1
             out.write("] ] ")
             if punc0>0:
                 out.write(", ")
